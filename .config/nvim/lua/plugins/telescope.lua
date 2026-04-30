faaa6ee395c328b4b0b3635f5141612432f2a3b8
Ryan Stelzleni <rstelzleni@gmail.com>
2026-04-29 18:49:59 -0700

    update for nvim v0.12.2

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
+        tag = 'v0.2.1',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {'nvim-tree/nvim-web-devicons'}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,5 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
+    branch = 'main',
+    build = ':TSUpdate'
 }
